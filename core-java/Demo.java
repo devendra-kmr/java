57f5489755e1522e51b9ad7cde56a5fa38b738f9
Devendra <devendrakumar.gzp@outlook.com>
2018-01-29 23:57:58 +0530

    added new class demo

---
+++ core-java/Demo.java
@@ -0,0 +1,24 @@
+package core;
+
+public class Demo {
+	
+	
+	public static void main(String[] args) {
+		sum(10,20);
+	}
+	
+    public static void sum(long l1,long l2){
+    	
+    	System.out.println("Long Method");
+		
+	}
+	public static void sum(float f1,float f2){
+		
+		System.out.println("Float Method");
+		
+	}
+
+}
+
+
+// Output:   Long Method
