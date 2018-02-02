9d618fbfa58682a648fb686f51065bbdff89773e
Devendra <devendrakumar.gzp@outlook.com>
2018-02-02 23:38:54 +0530

    modified

--- core-java/src/main/java/com/devendra/sample/Demo.java
+++ core-java/src/main/java/com/devendra/sample/Demo.java
@@ -2,11 +2,9 @@ package com.devendra.sample;
 
 public class Demo {
 	
-	
 	public static void main(String[] args) {
 		sum(10,20);
 	}
-	
     public static void sum(long l1,long l2){
     	
     	System.out.println("Long Method");
