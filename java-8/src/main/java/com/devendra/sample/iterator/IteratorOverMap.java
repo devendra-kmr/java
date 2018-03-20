caa36c2fc388d0aaf0a3f0ee27e5b9f636a8ac4b
Devendra <devendrakumar.gzp@outlook.com>
2018-03-20 21:46:21 +0530

    added sample

---
+++ java-8/src/main/java/com/devendra/sample/iterator/IteratorOverMap.java
@@ -0,0 +1,25 @@
+package com.devendra.sample.iterator;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class IteratorOverMap {
+
+    public static void main(String[] args) {
+
+        Map<String, String> countryMap = new HashMap<>();
+        countryMap.put("1", "India");
+        countryMap.put("2", "Bhutan");
+        countryMap.put("3", "China");
+        countryMap.put("4", "Japan");
+        countryMap.put("5", "Rasia");
+
+
+        countryMap.forEach((key,value)->{
+            System.out.println(key);
+            System.out.println(value);
+
+        });
+
+    }
+}
