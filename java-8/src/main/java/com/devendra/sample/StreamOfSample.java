bccd86fbdfd9beb03203dcea37822403618f6b60
Devendra Kumar <devendrakumar.address@gmail.com>
2018-03-24 22:29:53 +0530

    Update StreamOfSample.java

--- java-8/src/main/java/com/devendra/sample/StreamOfSample.java
+++ java-8/src/main/java/com/devendra/sample/StreamOfSample.java
@@ -4,7 +4,6 @@ import java.util.stream.Stream;
 
 public class StreamOfSample{
 
-
 public static void main (String[] args){
 
     Stream.of("Apple","Banana","Mango","Guava").filter(s -> s.startsWith("B"))
